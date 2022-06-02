7fec6d30c25e521f7bb74b6fa42ac69f37c52334
gaurya7 <51187549+gaurya7@users.noreply.github.com>
2022-06-02 13:56:41 +0530

    Individual Codes for Eye, Neck and Jaw movements

---
+++ jaw.ino
@@ -0,0 +1,24 @@
+
+
+#include <Servo.h>
+
+Servo servoJaw;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  servoJaw.attach(9);  // attaches the servo on pin 9 to the servo object
+}
+
+void loop() {
+  for (pos = 0; pos <= 40; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    servoJaw.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15 ms for the servo to reach the position
+  }
+  for (pos = 40; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    servoJaw.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15 ms for the servo to reach the position
+  }
+}
